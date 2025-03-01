67d71669b30fdddbd832dd35f63626819d5ae1e3
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-03-01 12:21:40 +0530

    Saurabh Add . random dice number from 1 to 6

---
+++ Sequence_Practice_Problems/diceNumber.js
@@ -0,0 +1,7 @@
+// diceRoll.js
+
+// Generate a random dice number between 1 and 6
+let diceNumber = Math.floor(Math.random() * 6) + 1;
+
+// Print the result
+console.log("Rolled Dice Number:", diceNumber);
