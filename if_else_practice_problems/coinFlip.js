2821d4e7d9f76b99a21a5e9326e7d1af0f2643cb
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-03-01 13:12:46 +0530

    Saurabh Add . simulate coin flip and print Heads or Tails

---
+++ if_else_practice_problems/coinFlip.js
@@ -0,0 +1,9 @@
+// Simulate a coin flip and print "Heads" or "Tails"
+
+let randomValue = Math.random();
+
+if (randomValue < 0.5) {
+    console.log("Heads");
+} else {
+    console.log("Tails");
+}
